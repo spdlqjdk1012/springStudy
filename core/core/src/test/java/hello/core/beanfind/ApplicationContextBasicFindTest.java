af98a306d0b1a0c58f13d211d840d86ad2d32f82
blendingp <58261910+blendingp@users.noreply.github.com>
2022-02-26 20:56:15 +0900

    빈 검증

--- core/core/src/test/java/hello/core/beanfind/ApplicationContextBasicFindTest.java
+++ core/core/src/test/java/hello/core/beanfind/ApplicationContextBasicFindTest.java
@@ -3,8 +3,10 @@ package hello.core.beanfind;
 import hello.core.AppConfig;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.NoSuchBeanDefinitionException;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -16,7 +18,31 @@ public class ApplicationContextBasicFindTest {
     @DisplayName("빈 이름으로 조회")
     void findBeanByName(){
         MemberService memberService = ac.getBean("memberService", MemberService.class);
-        System.out.println("memberService = "+ memberService);
-        System.out.println("memberService.getClass() = "+ memberService.getClass());
+        assertThat(memberService).isInstanceOf(MemberServiceImpl.class);
+        // memberService가 MemberServiceImpl의 인스턴스이면 성공
     }
+
+    @Test
+    @DisplayName("이름 없이 타입으로만 조회")
+    void findBeanByType(){
+        MemberService memberService = ac.getBean(MemberService.class);
+        assertThat(memberService).isInstanceOf(MemberServiceImpl.class);
+    }
+
+    @Test
+    @DisplayName("구체 타입으로 조회")
+    void findBeanByName2(){
+        MemberServiceImpl memberService = ac.getBean("memberService", MemberServiceImpl.class);
+        assertThat(memberService).isInstanceOf(MemberServiceImpl.class); // 구현에 의존해야함 이건 역할에 의존
+    }
+
+    @Test
+    @DisplayName("빈 이름으로 조회X")
+    void findBeanByNameX(){
+        Assertions.assertThrows(NoSuchBeanDefinitionException.class,
+                () -> ac.getBean("xxxxx", MemberService.class));
+        // ac.getBean("xxxxx", MemberService.class) 이 로직을 실행하면 오류가 터져야 결과가 성공이다
+    }
+
+
 }
