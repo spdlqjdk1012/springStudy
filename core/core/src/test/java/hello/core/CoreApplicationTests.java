df8f58b5eced2aa848d822116eaa6f611b21a62b
blendingp <58261910+blendingp@users.noreply.github.com>
2022-02-26 15:11:54 +0900

    init
    새로운 할인정책 적용과 문제점

---
+++ core/core/src/test/java/hello/core/CoreApplicationTests.java
@@ -0,0 +1,13 @@
+package hello.core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+//스프링 띄우는 시간 때문에 오래걸림(스프링 라이브러리가 많아질 경우)
+@SpringBootTest
+class CoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ core/core/src/test/java/hello/core/member/MemberServiceTest.java
@@ -0,0 +1,20 @@
+package hello.core.member;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class MemberServiceTest {
+
+    MemberService memberService = new MemberServiceImpl();
+    //순수하게 자바코드로 단위테스트 하는 것이 중요
+    @Test
+    void join(){
+        //given
+        Member member = new Member(1L, "memberA", Grade.VIP);
+        //when
+        memberService.join(member);
+        Member findMember = memberService.findMember(1L);
+        //then
+        Assertions.assertThat(member).isEqualTo(findMember);
+    }
+}
