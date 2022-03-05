27f441a9113f1c62c25498893e68859c9c83aedb
spdlqjdk1012 <58261910+blendingp@users.noreply.github.com>
2022-03-06 02:04:37 +0900

    빈 등록 초기화, 소멸 메서드 지정

--- core/core/src/test/java/hello/core/lifecycle/NetworkClient.java
+++ core/core/src/test/java/hello/core/lifecycle/NetworkClient.java
@@ -1,9 +1,6 @@
 package hello.core.lifecycle;
 
-import org.springframework.beans.factory.DisposableBean;
-import org.springframework.beans.factory.InitializingBean;
-
-public class NetworkClient implements InitializingBean, DisposableBean {
+public class NetworkClient{
     private  String url;
     public NetworkClient(){
         System.out.println("생성자 호출 url = "+ url);
@@ -28,14 +25,12 @@ public class NetworkClient implements InitializingBean, DisposableBean {
     }
 
     //의존관계 주입이 끝나면 호출해준다
-    @Override
-    public void afterPropertiesSet() throws Exception {
+    public void init(){
         connect();
         call("초기화 연결 메시지");
     }
 
-    @Override
-    public void destroy() throws Exception {
+    public void close(){
         disconnect();
     }
 }
