4711ee5e84e06c31e65808c620ba851c489e05e1
spdlqjdk1012 <58261910+blendingp@users.noreply.github.com>
2022-03-06 00:06:41 +0900

    Qualifier 테스트

--- core/core/src/main/java/hello/core/discount/FixDiscountPlicy.java
+++ core/core/src/main/java/hello/core/discount/FixDiscountPlicy.java
@@ -2,8 +2,10 @@ package hello.core.discount;
 
 import hello.core.member.Grade;
 import hello.core.member.Member;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
-
+@Component
+@Qualifier("fixDiscountPolicy")
 public class FixDiscountPlicy implements DiscountPolicy{
     private  int discountFixAmount = 1000;
     @Override
